16861c90ab8c057f81b542d82af309bb9ea706ba
Beijing-corn <96094900+Beijing-corn@users.noreply.github.com>
2023-01-29 20:32:04 -0700

    Add files via upload

---
+++ assess.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {ScrollFunction()}
+
+function ScrollFunction(){
+    if(document){
+document.getElementById("nav").style.background-color ; "white"
+    }
+    else{
+
+    }
+}
